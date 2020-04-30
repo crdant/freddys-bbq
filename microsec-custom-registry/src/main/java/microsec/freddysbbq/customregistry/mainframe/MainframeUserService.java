5c3243c4f8b4a586475eb142fc0901ec619e3921
Chuck D'Antonio <cdantonio@pivotal.io>
2020-04-30 09:28:43 -0400

    Create version branch
    TL;DR
    -----
    
    Add a branch for tracking semantic versioning from Concourse
    
    Details
    -------
    
    The Cloud Pipelines use the `semver` resource for Concourse to
    track the version of the code in branch in the Git repo. This
    commit creates that branch and makes it available to the pipeline.
    
    To keep things tidy, I deleted all other files and updated the
    `.gitignore` so that only the file `version` is allow on this
    branch.

--- microsec-custom-registry/src/main/java/microsec/freddysbbq/customregistry/mainframe/MainframeUserService.java
+++
@@ -1,7 +0,0 @@
-package microsec.freddysbbq.customregistry.mainframe;
-
-import java.util.Optional;
-
-public interface MainframeUserService {
-	public Optional<MainframeUser> getUserDetails(String username);
-}
--- microsec-menu-service/src/main/java/microsec/freddysbbq/menu/MenuItemRepository.java
+++
@@ -1,8 +0,0 @@
-package microsec.freddysbbq.menu;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import microsec.freddysbbq.menu.model.v1.MenuItem;
-
-public interface MenuItemRepository extends PagingAndSortingRepository<MenuItem, Long> {
-}
